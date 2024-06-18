21a800a16eea42396440f17aaf544a12f8fc2ffd
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-06-18 18:07:13 +0530

    Updated the Statistics as Provided

--- src/components/menu/home/components/Stats.jsx
+++ src/components/menu/home/components/Stats.jsx
@@ -1,25 +1,37 @@
 const Stats = () => {
     return (
         <div className="bg-base-300">
-            <div className="px-0 md:px-44 pt-5">
+            {/* <div className="px-0 md:px-44 pt-5">
                 <div className="text-3xl text-center"><span className="font-bold">Our Stats</span></div>
-            </div>
+            </div> */}
             <div className="grid grid-cols-1 md:grid-cols-4 gap-5 text-center py-14">
                 <div>
-                    <h2 className="text-3xl font-bold pb-2">15+</h2>
+                    <div className="flex justify-center items-center gap-2 mb-2">
+                        <h2 className="text-3xl font-bold pb-1">16+</h2>
+                        {/* <div className="badge badge-secondary">+1</div> */}
+                    </div>
                     <p className="text-xl font-bold">Clients</p>
                 </div>
                 <div>
-                    <h2 className="text-3xl font-bold pb-2">130+</h2>
+                    <div className="flex justify-center items-center gap-2 mb-2">
+                        <h2 className="text-3xl font-bold pb-1">182+</h2>
+                        {/* <div className="badge badge-secondary">+52</div> */}
+                    </div>
                     <p className="text-xl font-bold">Courses</p>
                 </div>
                 <div>
-                    <h2 className="text-3xl font-bold pb-2">225000+</h2>
-                    <p className="text-xl font-bold">Certifications</p>
+                    <div className="flex justify-center items-center gap-2 mb-2">
+                        <h2 className="text-3xl font-bold pb-1">3,39,628+</h2>
+                        {/* <div className="badge badge-secondary">+1,14,628</div> */}
+                    </div>
+                    <p className="text-xl font-bold">Users</p>
                 </div>
                 <div>
-                    <h2 className="text-3xl font-bold pb-2">270000+</h2>
-                    <p className="text-xl font-bold">Users</p>
+                    <div className="flex justify-center items-center gap-2 mb-2">
+                        <h2 className="text-3xl font-bold pb-1">6,83,864+</h2>
+                        {/* <div className="badge badge-secondary">+4,13,864</div> */}
+                    </div>
+                    <p className="text-xl font-bold">Certifications</p>
                 </div>
             </div>
         </div>
